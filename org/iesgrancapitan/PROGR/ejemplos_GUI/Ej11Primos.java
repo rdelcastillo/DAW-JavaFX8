f0d4b8d7c732d63d49a6b3a5a2e89391260f304b
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2020-05-05 19:04:13 +0200

    Ejemplo de uso de FXML con controles básicos y clase controladora

--- org/iesgrancapitan/PROGR/ejemplos_GUI/Ej11Primos.java
+++ org/iesgrancapitan/PROGR/ejemplos_GUI/Ej11Primos.java
@@ -9,18 +9,20 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Ej11Primos extends Application {
 
   @Override
   public void start(Stage primaryStage) throws IOException {
-    FXMLLoader fxml = new FXMLLoader(this.getClass().getResource("vistas/Ej11Primos.fxml"));
-    GridPane root = fxml.<GridPane>load();  
+    // Obtenemos nodo raíz del FXML (otra forma)
+    Parent root = FXMLLoader.load(getClass().getResource("vistas/Ej11Primos.fxml"));
+    
+    // Añadimos nodo a una escena y esta al escenario
     primaryStage.setScene(new Scene(root));
-    primaryStage.setTitle("App " + this.getClass().getSimpleName());     // nombre de la clase
+    primaryStage.setTitle("App " + this.getClass().getSimpleName());
     primaryStage.show();
   }
 
